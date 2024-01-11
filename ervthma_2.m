27db280a3cc7ba72cca69a65d566f56b4a8ed320
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2024-01-11 11:40:55 +0200

    Update ervthma_2.m

--- ervthma_2.m
+++ ervthma_2.m
@@ -1,15 +1,24 @@
 % Load the image
 img = imread("parrot.png");
 % get unique values
-uniqueValues = nchoosek(unique(img(:)), 2);
+uniqueValues = unique(img(:));
+numValues = length(uniqueValues);
+
+orderedPairs = [];
+for i = 1:numValues
+    for j = 1:numValues
+        orderedPairs = [orderedPairs; uniqueValues(i), uniqueValues(j)];
+    end
+end
+
 % get the frequency of values
-counts = zeros(size(uniqueValues,1),1);
+counts = zeros(size(orderedPairs,1),1);
 [m, n] = size(img);
 for col=1:n
     currentColumn = img(:,col);
-    for c=1:size(uniqueValues,1)
+    for c=1:size(orderedPairs,1)
         for ro=1:2:m
-            if(uniqueValues(c,1)==currentColumn(ro) && uniqueValues(c,2)==currentColumn(ro+1))
+            if(orderedPairs(c,1)==currentColumn(ro) && orderedPairs(c,2)==currentColumn(ro+1))
                 counts(c)=counts(c)+1;
             end 
         end
@@ -18,15 +27,15 @@ end
 % normalize frequencies , it is probability now
 probs = counts/sum(counts(:));
 disp('Unique Values');
-disp(uniqueValues');
+disp(orderedPairs');
 disp('Counts of Unique Values');
 disp(counts');
 disp('Probability of Unique Values');
 disp(probs');
 % Create custom labels for pairs
-symbolLabels = cell(size(uniqueValues, 1), 1);
-for i = 1:size(uniqueValues, 1)
-    symbolLabels{i} = [num2str(uniqueValues(i, 1)),num2str(uniqueValues(i, 2))];
+symbolLabels = cell(size(orderedPairs, 1), 1);
+for i = 1:size(orderedPairs, 1)
+    symbolLabels{i} = [num2str(orderedPairs(i, 1)),num2str(orderedPairs(i, 2))];
 end
 % Compute Huffman dictionary
 huffmanDict = huffmandict(symbolLabels, probs);
